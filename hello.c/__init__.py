48a22a23100608ffce437921446732fc46acc36a
Saabir14 <64490729+Saabir14@users.noreply.github.com>
2021-11-30 20:10:19 +0400

    Create __init__.py

---
+++ hello.c/__init__.py
@@ -0,0 +1,16 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+  """hello.c Exists"""
+  check50.exists("hello.c")
+  
+@check50.check(exists)
+  """hello.c compiles"""
+  check50.c.compile("hello.c")
+  
+@check50.check(compiles)
+def prints_hello():
+  """prints "hello, world\\n" """
+  check50.run("./hello").stdout("[Hh]ello, [Ww]orld!?\n", regex=True).exit(0)
